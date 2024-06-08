c2a8223236ff875a10b772068adff2421f3dd4c0
ekovalenko <eduard.kovalenko@cleverbridge.com>
2024-06-08 12:20:52 +0300

    feature: Client side - Add basic views - Setup project - Create navbar component - Setup tailwind.css

---
+++ nutrihelp_client/src/components/NavItem.vue
@@ -0,0 +1,35 @@
+<template>
+    <li 
+
+      class="text-md no-underline text-white hover:text-blue-dark ml-2 px-1 text-nowrap self-center"
+      :class="{ 'bg-gray-700': isActive }" 
+      @click="$emit('click')"
+    >
+      {{ label }}
+    </li>
+  </template>
+  
+  <script>
+  export default {
+    name: 'NavItem',
+    props: {
+      label: {
+        type: String,
+        required: true
+      },
+      route: {
+        type: String,
+        required: true
+      },
+      isActive: {
+        type: Boolean,
+        required: true
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  /* Additional styles if needed */
+  </style>
+  
